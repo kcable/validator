5adeb69726e9ea64607f36626d8be22c85238f88
Dimitar K. Nikolov <dknikolov@gmail.com>
2020-11-30 15:24:42 +0200

    chore: test GitLab pipeline variables

--- cypress/integration/task-1.js
+++ cypress/integration/task-1.js
@@ -1,28 +1,32 @@
 context('task-1', () => {
-  before(() => {
-    cy.task('gitClone', {
-      url: Cypress.env('REPO_URL'),
-      dir: "./.tmp"
-    })
-      .then((data) => {
-        expect(data).to.be.true;
-      });
+  // before(() => {
+  //   cy.task('gitClone', {
+  //     url: Cypress.env('REPO_URL'),
+  //     dir: "./.tmp"
+  //   })
+  //     .then((data) => {
+  //       expect(data).to.be.true;
+  //     });
+  // });
+
+  it('should get the pipeline variable', () => {
+    expect(Cypress.env('TEST_VARIABLE')).to.not.be.undefined;
   });
 
-  it('has 4 commits', () => {
-    cy.task('gitLog', {
-      dir: './.tmp'
-    }).then((data) => {
-      expect(data.length).to.equal(1);
-    })
-  });
-
-  after(() => {
-    cy.task('rmDir', {
-      dir: './.tmp'
-    })
-      .then((data) => {
-        expect(data).to.be.true;
-      });
-  });
+  // it('has 4 commits', () => {
+  //   cy.task('gitLog', {
+  //     dir: './.tmp'
+  //   }).then((data) => {
+  //     expect(data.length).to.equal(1);
+  //   })
+  // });
+  //
+  // after(() => {
+  //   cy.task('rmDir', {
+  //     dir: './.tmp'
+  //   })
+  //     .then((data) => {
+  //       expect(data).to.be.true;
+  //     });
+  // });
 });
