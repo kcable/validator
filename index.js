ca9b8a3f4536636eb691b8ef8839b391528489cc
Dimitar K. Nikolov <dknikolov@gmail.com>
2020-12-04 13:35:20 +0200

    feat: add testing of dynamic tasks

--- index.js
+++ index.js
@@ -6,15 +6,19 @@ const { exec } = require('child_process');
 
 // run project - npm start
 
-console.log('Testing...');
-
 if (!process.env.RUN) {
   process.exit(0);
 }
 
-const output = exec('npm run test -- --spec "cypress/integration/task-1.js"', {
-  env: process.env
-});
+const taskId = process.env.TASK_ID;
+
+console.log(`Running tests for task ${taskId}...`);
+
+const output = exec(
+  `npm run test -- --spec "cypress/integration/task-${process.env.TASK_ID}.js`, {
+    env: process.env
+  }
+);
 
 output.stdout.on('data', (data) => {
   console.log(data);
