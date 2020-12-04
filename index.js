397efdefecc5132ac70068b367c136a6d97c7a0c
Dimitar K. Nikolov <dknikolov@gmail.com>
2020-12-04 14:44:29 +0200

    refactor: add taskId to command string

--- index.js
+++ index.js
@@ -1,11 +1,5 @@
 const { exec } = require('child_process');
 
-// clone project - Project url should be passed in trough gitlab. Project should be cloned in /temp
-
-// install project - npm i
-
-// run project - npm start
-
 if (!process.env.RUN) {
   process.exit(0);
 }
@@ -15,7 +9,7 @@ const taskId = process.env.TASK_ID;
 console.log(`Running tests for task ${taskId}...`);
 
 const output = exec(
-  `npm run test -- --spec "cypress/integration/task-${taskId}.js`, {
+  `npm run test -- --spec "cypress/integration/task-${taskId}.js"`, {
     env: process.env
   }
 );
