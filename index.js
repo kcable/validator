1b9225bb8f39f7a63df8b93cc40f2273051dc966
Dimitar K. Nikolov <dknikolov@gmail.com>
2020-12-04 13:39:26 +0200

    refactor: add better usage of dynamic task id

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ const taskId = process.env.TASK_ID;
 console.log(`Running tests for task ${taskId}...`);
 
 const output = exec(
-  `npm run test -- --spec "cypress/integration/task-${process.env.TASK_ID}.js`, {
+  `npm run test -- --spec "cypress/integration/task-${taskId}.js`, {
     env: process.env
   }
 );
