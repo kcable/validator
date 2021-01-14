7353ece54a863e1854ab6ac11ae0204b1ac2f4e6
Dimitar K. Nikolov <dknikolov@gmail.com>
2021-01-14 11:27:18 +0200

    fix: always passing 401 unauthorized repos

--- index.js
+++ index.js
@@ -14,32 +14,37 @@ const dir = `./.tmp`;
 if (!env.RUN) process.exit(0);
 
 (async () => {
-  console.log(`Initiating tests for task ${taskId}...`);
-  console.log(` DIRECTORY: ${dir}`);
+  try {
+    console.log(`Initiating tests for task ${taskId}...`);
+    console.log(` DIRECTORY: ${dir}`);
 
-  await clone({ dir, url });
-  await install({ dir });
+    await clone({ dir, url });
+    await install({ dir });
 
-  // Run the start script if the project has one
-  const package = require(`${dir}/package.json`);
-  if(package.scripts.start) await start({ dir });
+    // Run the start script if the project has one
+    const package = require(`${dir}/package.json`);
+    if(package.scripts.start) await start({ dir });
 
-  console.log(`Starting to run tests...`);
-  // The Cypress task is a bit weird that's why we're using outside of primises 
-  const cmd = open ? `npm run cypress:open` : `npm run test -- ${headed} --spec "cypress/integration/task-${taskId}.js"`;
-  const output = exec(cmd, { env: process.env });
-  
-  output.stdout.on('data', (data) => {
-    console.log(data);
+    console.log(`Starting to run tests...`);
+    // The Cypress task is a bit weird that's why we're using outside of primises
+    const cmd = open ? `npm run cypress:open` : `npm run test -- ${headed} --spec "cypress/integration/task-${taskId}.js"`;
+    const output = exec(cmd, { env: process.env });
 
-    // We have to exit manually because the task doesn't do it itself
-    if(data.includes('Run Finished')) {
-      process.exit(0);
-    }
-  });
-  
-  output.stderr.on('data', (data) => {
-    console.log(data);
+    output.stdout.on('data', (data) => {
+      console.log(data);
+
+      // We have to exit manually because the task doesn't do it itself
+      if(data.includes('Run Finished')) {
+        process.exit(0);
+      }
+    });
+
+    output.stderr.on('data', (data) => {
+      console.log(data);
+      process.exit(1);
+    });
+  } catch (error) {
+    console.error(error);
     process.exit(1);
-  });
+  }
 })();
