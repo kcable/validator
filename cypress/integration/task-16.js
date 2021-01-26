627c6d12834b48bcad800db024e3f8de3a0b7cf2
Hristiyan Ivanov <c.ivanov@dopamine.bg>
2021-01-26 14:31:18 +0200

    fix: check task-16 via commit message not SHA

--- cypress/integration/task-16.js
+++ cypress/integration/task-16.js
@@ -2,39 +2,40 @@ const CustomError = require("../support/errors/CustomError");
 
 context(`task-16`, () => {
 
-  it(`should have a first commit with a SHA of 2a0bd54ecbbcdc88a4eae7ed09b1b3aea7d57de4`, () => {
+  it(`should have a first commit with a commit message of 'fix: update console log'`, () => {
     cy.task('gitLog', { ref: 'origin/dev' }).then((data) => {
       cy.log('Git Log', data);
+      cy.log(data[0]);
 
       expect(
-        data[0].oid,
+        data[0].commit.message,
         new CustomError({ 
-          issue: 'Не е cherry pick-нат commit-a с SHA 2a0bd54ecbbcdc88a4eae7ed09b1b3aea7d57de4 в dev бранча', 
+          issue: `Не е cherry pick-нат commit-a с message 'fix: update console log' в dev бранча`, 
           tips: [
             'Увери се, че си превключил на dev branch-a преди да cherry pick-неш', 
             'Увери се, че си cherry pick-нал commit-ите последователно както са в master',
             'Увери се, че наистина си import-нал проекта описан в task-a',
           ] 
         })
-      ).to.equal('2a0bd54ecbbcdc88a4eae7ed09b1b3aea7d57de4');
+      ).to.contain('fix: update console log');
     });
   });
 
-  it(`should have a second commit with a SHA of 235574808aa7641b045059e3310b873071a54578`, () => {
+  it(`should have a second commit with a commit message of 'fix: change project name'`, () => {
     cy.task('gitLog', { ref: 'origin/dev' }).then((data) => {
       cy.log('Git Log', data);
 
       expect(
-        data[1].oid,
+        data[1].commit.message,
         new CustomError({ 
-          issue: 'Не е cherry pick-нат commit-a с SHA 2a0bd54ecbbcdc88a4eae7ed09b1b3aea7d57de4 в dev бранча', 
+          issue: `Не е cherry pick-нат commit-a с message 'fix: change project name' в dev бранча`, 
           tips: [
             'Увери се, че си превключил на dev branch-a преди да cherry pick-неш', 
             'Увери се, че си cherry pick-нал commit-ите последователно както са в master',
             'Увери се, че наистина си import-нал проекта описан в task-a',
           ] 
         })
-      ).to.equal('235574808aa7641b045059e3310b873071a54578');
+      ).to.contain('fix: change project name');
     });
   });
   
